628840f2a6e227bf3172c026d426cc7e60441598
JackM <jackm@Jacks-MBP.fios-router.home>
2024-06-03 19:30:37 -0400

    final commit

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -34,7 +34,7 @@ export const PATCH = async (request, { params }) => {
 
         return new Response(JSON.stringify(existingPrompt), {status: 200});
     }catch (error){
-        return new Response("Failed to update prompt", { status: 500 })
+        return new Response("Failed to update prompt", error.message, { status: 500 })
     }
 }
 
